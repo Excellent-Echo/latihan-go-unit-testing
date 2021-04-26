637f14268f2756de474b35921417689e3f8c701a
mohammedazez <muhamadaziz047@gmail.com>
2021-04-26 11:46:22 +0700

    done

--- main.go
+++ main.go
@@ -2,47 +2,81 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/leekchan/accounting"
+	"goAdvance/helper"
 )
 
-func SayHello(name string) string {
-	if name == "" {
-		return "Hello, friend"
-	} else if len(name) > 5 {
-		return "Hello, long name"
-	} else {
-		return "Hello, " + name
-	}
-}
-
-func HelloHello() string {
-	return "Hello hello"
-}
-
-func Calculate(num1 int, num2 int) int {
-	return num1 + num2
-}
-
 func main() {
-	// 3rd party
+	// Latihan 1
+	helper.Total(110000)
+	helper.Total(220000)
+	helper.Total(500000)
+	helper.Total(1000000)
+	helper.Total(0)
 
-	ac := accounting.Accounting{
-		Symbol:    "Rp",
-		Precision: 2,
+	fmt.Println("-----------------------------------")
+
+	//	latihan 2
+	fmt.Println(helper.ChangeToIdr(100000))
+	fmt.Println(helper.ChangeToIdr(1200000))
+	fmt.Println(helper.ChangeToIdr(140350000))
+	fmt.Println(helper.ChangeToIdr(0))
+
+	fmt.Println("-----------------------------------")
+
+	// latihan 3
+	fmt.Println(helper.GenapGanjil(1, 2, 3, 4, 5))
+	fmt.Println(helper.GenapGanjil(4, 2))
+	fmt.Println(helper.GenapGanjil(10, 20, 30, 13))
+	fmt.Println(helper.GenapGanjil(30, 13, 13, 77, 33, 55, 17, 13))
+	fmt.Println(helper.GenapGanjil())
+
+	fmt.Println("-----------------------------------")
+
+	// latihan 4
+	var data = map[string]string{
+		"name":        "andi",
+		"umur":        "30",
+		"jarakRumah":  "50",
+		"berkeluarga": "ya",
+	}
+	var data2 = map[string]string{
+		"name":        "santi",
+		"umur":        "19",
+		"jarakRumah":  "80",
+		"berkeluarga": "ya",
+	}
+	var data3 = map[string]string{
+		"name":        "budi",
+		"umur":        "45",
+		"jarakRumah":  "120",
+		"berkeluarga": "ya",
+	}
+	fmt.Println(helper.GovermentHelper(data))
+	fmt.Println(helper.GovermentHelper(data2))
+	fmt.Println(helper.GovermentHelper(data3))
+	fmt.Println("-----------------------------------")
+
+	// latihan 5
+	helper.ChangeNumtoStr(100000)
+	helper.ChangeNumtoStr(111000)
+	helper.ChangeNumtoStr(5124000)
+	helper.ChangeNumtoStr(1543)
+	helper.ChangeNumtoStr(1234678)
+	helper.ChangeNumtoStr(0)
+
+	fmt.Println("-----------------------------------")
+
+	// latihan 6
+	var belanja1 = map[string]int{
+		"sepatu":   1100000,
+		"jaket":    2200000,
+		"topi":     594000,
+		"celana":   803000,
+		"sweater":  330000,
+		"kausKaki": 110000,
+		"sabuk":    55000,
 	}
 
-	fmt.Println(ac.FormatMoney(100000))
+	fmt.Println(helper.TotalPajak(belanja1))
 
-	// result, err := calc.Calculator(1, 2, "add")
-
-	// greeting := hello.Hello()
-
-	// if err != nil {
-	// 	fmt.Println(err.Error())
-	// 	return
-	// }
-
-	// fmt.Println(greeting)
-	// fmt.Println(result)
 }
